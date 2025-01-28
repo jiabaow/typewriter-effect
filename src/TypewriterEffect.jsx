a2a9d9bc7a65650df7c7fe9b7d27158519509b34
jiabaow <jiabaowenwendy@gmail.com>
2025-01-27 18:44:08 -0800

    finish

--- src/TypewriterEffect.jsx
+++ src/TypewriterEffect.jsx
@@ -1,10 +1,44 @@
+import {useEffect, useRef, useState} from "react";
+
 const TypewriterEffect = () => {
+    const [typedSentence, setTypedSentence] = useState('');
+    const intervalId = useRef();
+    const savedCallback = useRef();
+
+    const startTyping = (sentence) => {
+        setTypedSentence("");
+        intervalId.current = setInterval(() => {
+            savedCallback.current(sentence);
+        }, 500);
+    };
+
+    const stopTyping = () => {
+        clearInterval(intervalId.current);
+        intervalId.current = null;
+    }
+
+    const typedNextLetter = (sentence) => {
+        const letters = Array.from(sentence);
+        if (typedSentence.length === letters.length) {
+            stopTyping();
+            return;
+        }
+         const nextLetterIdx = typedSentence.length;
+        setTypedSentence(typedSentence + letters[nextLetterIdx]);
+    }
+
+    useEffect(() => {
+        savedCallback.current = typedNextLetter;
+    });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     // TODO Display the text with typewriter effect
+    startTyping(data.get("sentence"));
     console.log(`The sentence to display is ${data.get("sentence")}`);
   };
+
   return (
     <div>
       <form
@@ -22,6 +56,7 @@ const TypewriterEffect = () => {
         />
         <button type="submit">Display with typewriter effect</button>
       </form>
+        {typedSentence && <p>You typed {typedSentence}</p>}
     </div>
   );
 };
